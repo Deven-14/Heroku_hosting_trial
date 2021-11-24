ea5dd146ad3db448e10a39006534f3dbead0337a
Deven-14 <deven.paramaj@gmail.com>
2021-11-25 00:10:53 +0530

    added ejs

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    //res.send("Hello world");
+    res.render("index.ejs");
 });
 
 app.listen(process.env.PORT || port, () => {
