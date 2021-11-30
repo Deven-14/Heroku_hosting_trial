8211edc43580c403dbb259f7bf53b0dbde3ebcc2
Deven-14 <deven.paramaj@gmail.com>
2021-12-01 00:56:19 +0530

    removed post method

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    //res.send("Hello world");
-    res.render("index.ejs");
-});
-
-app.post('/', (req, res) => {
     //res.send("Hello world");
     res.render("index.ejs");
 });
